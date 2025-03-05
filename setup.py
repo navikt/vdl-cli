ce169bf4c92b2a52893bf56012087e8a9777dfff
Patrick Pedersen <patrick.pedersen@nav.no>
2025-03-05 15:23:01 +0100

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="vdc",
-    version="0.0.2",
+    version="0.0.3",
     description=(
         "CLI tool for helping with daily tasks in Virksomhetsdatalaget"
     ),
