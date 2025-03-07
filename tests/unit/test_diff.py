d47b66dc29215676dff3bbc2629a16e929c3e587
magnusborsting <82023189+magnusborsting@users.noreply.github.com>
2025-03-07 11:46:15 +0100

    Legger til primary key i testene

--- tests/unit/test_diff.py
+++ tests/unit/test_diff.py
@@ -10,30 +10,28 @@ class TestTableDiff(unittest.TestCase):
         prod_df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
         dev_df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
 
-        result = _compare_df(prod_df, dev_df, "prod", "dev")
+        result = _compare_df(prod_df, dev_df, "prod", "dev", "a")
 
         self.assertTrue(result.empty)
 
     def test_compare_df_with_diff(self):
-        prod_df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}).set_index("a")
-        dev_df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 7]}).set_index("a")
+        prod_df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+        dev_df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 7]})
 
-        result = _compare_df(prod_df, dev_df, "prod", "dev")
-        expected = (
-            pd.DataFrame({"a": [3, 3], "b": [6.0, 7.0], "result_name": ["prod", "dev"]})
-            .set_index(["a", "result_name"])
-        )
+        result = _compare_df(prod_df, dev_df, "prod", "dev", "a")
+        expected = pd.DataFrame(
+            {"a": [3, 3], "b": [6.0, 7.0], "result_name": ["prod", "dev"]}
+        ).set_index(["a", "result_name"])
         self.assertTrue(result.equals(expected))
 
     def test_compare_df_with_one_more_row_in_prod(self):
-        prod_df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}).set_index("a")
-        dev_df = pd.DataFrame({"a": [1, 2], "b": [4, 5]}).set_index("a")
+        prod_df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+        dev_df = pd.DataFrame({"a": [1, 2], "b": [4, 5]})
 
-        result = _compare_df(prod_df, dev_df, "prod", "dev")
-        expected = (
-            pd.DataFrame({"a": [3], "b": [6.0], "result_name": ["prod"]})
-            .set_index(["a", "result_name"])
-        )
+        result = _compare_df(prod_df, dev_df, "prod", "dev", "a")
+        expected = pd.DataFrame(
+            {"a": [3], "b": [6.0], "result_name": ["prod"]}
+        ).set_index(["a", "result_name"])
         self.assertTrue(result.equals(expected))
 
 
