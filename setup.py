761d54986230c17fae37607ffc529a16196dc7c1
magnusborsting <82023189+magnusborsting@users.noreply.github.com>
2025-03-10 10:44:11 +0100

    Oppdaterer versjonsnummer

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="vdl-cli",
-    version="0.1.7",
+    version="0.1.8",
     description=("CLI tool for helping with daily tasks in Virksomhetsdatalaget"),
     packages=find_packages(include=("vdc/*,")),
     author="NAV IT Virksomhetsdatalaget",
