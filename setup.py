a215ff6dc5138e15e7321cd820dfc182f48262d3
Patrick Pedersen <patrick.pedersen@nav.no>
2025-03-19 18:25:32 +0100

    chore: bump version to 0.1.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="vdl-cli",
-    version="0.1.10",
+    version="0.1.11",
     description=("CLI tool for helping with daily tasks in Virksomhetsdatalaget"),
     packages=find_packages(include=("vdc/*,")),
     author="NAV IT Virksomhetsdatalaget",
