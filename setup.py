873fb5fb112ea042602e9b5f37ca358c40de8552
trineno <161321609+trineno@users.noreply.github.com>
2025-03-26 16:14:20 +0100

    chore: update version to 0.1.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="vdl-cli",
-    version="0.1.15",
+    version="0.1.16",
     description=("CLI tool for helping with daily tasks in Virksomhetsdatalaget"),
     packages=find_packages(include=("vdc/*,")),
     package_data={"vdc": ["banner.txt"]},
