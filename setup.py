c7db79ccedc0e934c553bf1849c92e91fa895b14
Cecilie Widsteen <cecilidw@gmail.com>
2025-08-22 16:00:37 +0200

    chore: bump version to 0.1.26

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="vdl-cli",
-    version="0.1.25",
+    version="0.1.26",
     description=("CLI tool for helping with daily tasks in Virksomhetsdatalaget"),
     packages=find_packages(include=("vdc/*,")),
     package_data={"vdc": ["banner.txt"]},
